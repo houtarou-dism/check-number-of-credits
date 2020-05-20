6f3a9104c19b9696314833a305b4996e73a61ad8
houtarou-dism <0897houtarou@gmail.com>
2020-05-21 01:58:09 +0900

    単位数チェック処理の追加

--- app/Http/Controllers/FreshmanController.php
+++ app/Http/Controllers/FreshmanController.php
@@ -2,11 +2,17 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\CheckCredit;
+use Illuminate\Support\Arr;
+use App\Traits\Jsonable;
+use App\Traits\Verifiable;
 use App\Http\Requests\CheckFormRequest;
+
 
 class FreshmanController extends Controller
 {
+    use Jsonable, Verifiable;
+
     public function freshman()
     {
         return view('university/freshman/freshman-freshman');
@@ -14,19 +20,18 @@ class FreshmanController extends Controller
 
     public function check(CheckFormRequest $request)
     {
-        //todo 必須単位で落単している科目，総取得数
-        $json = json_decode($request->data, true);
-        $selectYear = $json['data']['select'];
-        $data = $json['data']['freshman'];
+        //todo 必須科目で落単している科目，総取得数
+        $json = $this->toJson($request->data);
 
-        if($selectYear !== 'freshman'){
-            abort(404);
+        if(!Arr::has($json, 'data.freshman')){
+            return redirect(route('index'))->with('message', '不正な操作が行われました。');
         }
 
+        $data = $this->getData($json, 'freshman');
 
-        $totalCredits = count($data);       //修得済単位数
+        $notCompulsorySubjects = $this->requiredFreshman($data);         //必修科目だが、落としている単位
+        $totalCredits = count($data) * 2;       //修得済単位数（１科目２単位）
 
-
-        return view('university/result/freshman-result');
+        return view('university/result/freshman-result', compact('notCompulsorySubjects', 'totalCredits'));
     }
 }
--- app/Http/Controllers/SophomoreController.php
+++ app/Http/Controllers/SophomoreController.php
@@ -2,23 +2,47 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Arr;
+use App\Traits\Jsonable;
+use App\Traits\Verifiable;
+use App\Http\Requests\CheckFormRequest;
 
-use Illuminate\Http\Request;
 
 class SophomoreController extends Controller
 {
+    use Jsonable, Verifiable;
+
     public function freshman()
     {
         return view('university.sophomore.sophomore-freshman');
     }
 
-    public function sophomore(Request $request)
+    public function sophomore()
     {
         return view('university.sophomore.sophomore-sophomore');
     }
 
-    public function check(Request $request)
+    public function check(CheckFormRequest $request)
     {
-        dd(($request));
+        //todo 必須科目で落単している科目，総取得数
+        $json = $this->toJson($request->data);
+
+        if(!Arr::has($json, 'data.freshman') && !Arr::has($json, 'data.sophomore')){
+            return redirect(route('index'))->with('message', '不正な操作が行われました。');
+        }
+
+        $notCompulsorySubjects = [];        //必修科目だが、落としている単位
+
+        foreach ($this->selectYears as $selectYear){
+            $this->data[$selectYear] =  $this->getData($json, $selectYear)  ?? [];
+            if($selectYear === 'sophomore') { break; }
+        }
+
+        $notFreshmanCompulsorySubjects = json_encode($this->requiredFreshman($this->data['freshman']));
+        $notSophomoreCompulsorySubjects = json_encode($this->requiredSophomore($this->data['sophomore']));
+
+        $totalCredits = (count($this->data['freshman']) + count($this->data['sophomore'])) * 2;       //修得済単位数（１科目２単位）
+
+        return view('university/result/sophomore-result', compact('notFreshmanCompulsorySubjects','notSophomoreCompulsorySubjects','totalCredits'));
     }
 }
